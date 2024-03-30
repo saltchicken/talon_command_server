253cb3526ef6e068d3e976bb04e79a05f3afee06
saltchicken <john.eicher89@gmail.com>
2024-03-30 13:09:08 -0700

    Add socket server

--- llm_main.py
+++ llm_main.py
@@ -6,6 +6,20 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 
+import socket
+
+def recv_all(sock, bufsize=4096):
+    """
+    Receive data from the socket until no more data is available.
+    """
+    data = b""
+    while True:
+        chunk = sock.recv(bufsize)
+        if not chunk:
+            break
+        data += chunk
+    return data
+
 llm = Ollama(model="llama2")
 
 # test = llm.invoke("What is a Module in Talon?")
@@ -34,5 +48,44 @@ document_chain = create_stuff_documents_chain(llm, prompt)
 retriever = vector.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-response = retrieval_chain.invoke({"input": "Give me an example of a capture in Python"})
-print(response["answer"])
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('0.0.0.0', 12345)
+print('Starting up on {} port {}'.format(*server_address))
+server_socket.bind(server_address)
+
+# Listen for incoming connections
+server_socket.listen(1)
+
+while True:
+    print('Waiting for a connection...')
+    connection, client_address = server_socket.accept()
+    
+    try:
+        print('Connection from', client_address)
+
+        received_data = recv_all(connection)
+        print('Received {!r}'.format(received_data))
+        
+        phrase = received_data.decode('utf-8')
+        
+        response = retrieval_chain.invoke({"input": phrase})
+        print(response["answer"])
+
+        # if received_data == b'minimize window':
+        #     pyautogui.hotkey('win', 'down')
+                
+    finally:
+        # Clean up the connection
+        connection.close()
+
+
+
+
+
+
+
+
+
+
