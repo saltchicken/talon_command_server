c35bb9273c07ea1b7b3efa5b3532a2682153c4e8
saltchicken <john.eicher89@gmail.com>
2024-04-01 18:13:57 -0700

    Add ability to update writer via socket

--- llm_main.py
+++ llm_main.py
@@ -6,6 +6,12 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 
+from typing import Iterable
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import BaseMessageChunk
+from langchain_core.runnables import RunnableGenerator
+
 import socket
 
 def recv_all(sock, bufsize=4096):
@@ -20,72 +26,118 @@ def recv_all(sock, bufsize=4096):
         data += chunk
     return data
 
-llm = Ollama(model="llama2")
-
-# test = llm.invoke("What is a Module in Talon?")
-# print(test)
-
-from langchain_community.document_loaders import WebBaseLoader
-loader = WebBaseLoader("https://talon.wiki/unofficial_talon_docs/")
-
-embeddings = OllamaEmbeddings()
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(docs)
-vector = FAISS.from_documents(documents, embeddings)
-
-prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
-
-<context>
-{context}
-</context>
-
-Question: {input}""")
-
-document_chain = create_stuff_documents_chain(llm, prompt)
-
-retriever = vector.as_retriever()
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the port
-server_address = ('0.0.0.0', 12345)
-print('Starting up on {} port {}'.format(*server_address))
-server_socket.bind(server_address)
-
-# Listen for incoming connections
-server_socket.listen(1)
-
-while True:
-    print('Waiting for a connection...')
-    connection, client_address = server_socket.accept()
+class ScreenWriterClient():
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+    def __enter__(self):
+        # TODO: Add try | except
+        self.client_socket.connect(('localhost', 12346))
+        return self
     
-    try:
-        print('Connection from', client_address)
-
-        received_data = recv_all(connection)
-        print('Received {!r}'.format(received_data))
+    def __exit__(self, *args, **kwargs):
+        self.client_socket.close()
         
-        phrase = received_data.decode('utf-8')
+    def write(self, message):
+        self.client_socket.send(message.encode())
         
-        response = retrieval_chain.invoke({"input": phrase})
-        print(response["answer"])
+        
+if __name__ == "__main__":
+    llm = Ollama(model="llama2")
 
-        # if received_data == b'minimize window':
-        #     pyautogui.hotkey('win', 'down')
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Your responses are terse."),
+        ("user", "{input}")
+    ])
+
+    def streaming_parse(chunks: Iterable[BaseMessageChunk]) -> Iterable[str]:
+        for chunk in chunks:
+            yield chunk
+
+    streaming_parse = RunnableGenerator(streaming_parse)
+
+
+    chain = prompt | llm | streaming_parse
+
+    # test = llm.invoke("What is a Module in Talon?")
+    # print(test)
+
+    # from langchain_community.document_loaders import WebBaseLoader
+    # loader = WebBaseLoader("https://talon.wiki/unofficial_talon_docs/")
+
+    # embeddings = OllamaEmbeddings()
+    # docs = loader.load()
+
+    # text_splitter = RecursiveCharacterTextSplitter()
+    # documents = text_splitter.split_documents(docs)
+    # vector = FAISS.from_documents(documents, embeddings)
+
+    # prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+    # <context>
+    # {context}
+    # </context>
+
+    # Question: {input}""")
+
+    # document_chain = create_stuff_documents_chain(llm, prompt)
+
+    # retriever = vector.as_retriever()
+    # retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    
+    writer = ScreenWriterClient()
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the port
+    # server_address = ('0.0.0.0', 12345)
+    server_address = ('127.0.0.1', 12345)
+
+    print('Starting up on {} port {}'.format(*server_address))
+    server_socket.bind(server_address)
+
+    # app = QApplication(sys.argv)
+    # window = OverlayWindow('yes', 50)
+    # window.show()
+    # sys.exit(app.exec_())
+    # queue = Queue()
+    # p = Process(target=write_to_screen_process, args=('', 50, queue))
+    # p.daemon = True
+    # p.start()
+    # controller = OverlayController(queue)
+    # embed()
+
+    # Listen for incoming connections
+    server_socket.listen(1)
+
+    while True:
+        print('Waiting for a connection...')
+        connection, client_address = server_socket.accept()
+        
+        try:
+            print('Connection from', client_address)
+
+            received_data = recv_all(connection)
+            print('Received {!r}'.format(received_data))
+            
+            phrase = received_data.decode('utf-8')
+            output = ''
+            for chunk in chain.stream(phrase):
                 
-    finally:
-        # Clean up the connection
-        connection.close()
+                # if '\n' in chunk:
+                #     chunk = chunk.replace('\n', '')
+                #     controller.clear()
+                output += chunk
+                writer.write(output)
+                # controller.append(chunk)
+                # print(chunk, end="", flush=True)
+            
+            # response = retrieval_chain.invoke({"input": phrase})
+            # print(response["answer"])
 
-
-
-
-
-
-
-
-
-
+            # if received_data == b'minimize window':
+            #     pyautogui.hotkey('win', 'down')
+                    
+        finally:
+            # Clean up the connection
+            connection.close()
