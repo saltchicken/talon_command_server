9bbceb3dcef58a1fcbbc8e726fcb0fa80147a8c3
saltchicken <john.eicher89@gmail.com>
2024-04-05 13:59:23 -0700

    Update to send llm output

--- jarvis_server.py
+++ jarvis_server.py
@@ -3,8 +3,22 @@ import threading
 from loguru import logger
 import json
 import queue
+
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+
+from typing import Iterable
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import BaseMessageChunk
+from langchain_core.runnables import RunnableGenerator
     
-def tasker_thread_handler(server_socket, task_queue):
+def tasker_thread_handler(server_socket, task_queue, llm_chain):
     while True:
         logger.debug("Tasker thread waiting for connection")
         conn, addr = server_socket.accept()
@@ -16,7 +30,21 @@ def tasker_thread_handler(server_socket, task_queue):
                 break
             packet = json.loads(data)
             if packet['type'] == 'phrase':
-                conn.sendall(data)
+                output = ''
+                for chunk in llm_chain.stream(packet['message']):
+                    
+                    # if '\n' in chunk:
+                    #     chunk = chunk.replace('\n', '')
+                    #     controller.clear()
+                    output += chunk
+                    try:
+                        # print('wrote', output)
+                        data = {"type": "phrase", "message": output}
+                        data_string = json.dumps(data)
+                        conn.sendall(data_string.encode())
+                    except Exception as e:
+                        print(e)
+                # conn.sendall(data)
             else:
                 logger.debug('This packet type has not been implemented')
         conn.close()
@@ -40,6 +68,24 @@ def talon_thread_handler(server_socket, task_queue):
                     logger.debug('This packet type has not been implemented yet')
         conn.close()
     logger.debug('talon thread done')
+    
+def setup_llm():
+    llm = Ollama(model="llama2")
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Your responses are terse."),
+        ("user", "{input}")
+    ])
+
+    def streaming_parse(chunks: Iterable[BaseMessageChunk]) -> Iterable[str]:
+        for chunk in chunks:
+            yield chunk
+
+    streaming_parse = RunnableGenerator(streaming_parse)
+
+
+    chain = prompt | llm | streaming_parse
+    return chain
 
 def main():
     tasker_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,7 +100,8 @@ def main():
     
     task_queue = queue.Queue()
     
-    tasker_thread = threading.Thread(target=tasker_thread_handler, args=(tasker_server_socket, task_queue))
+    llm_chain = setup_llm()
+    tasker_thread = threading.Thread(target=tasker_thread_handler, args=(tasker_server_socket, task_queue, llm_chain))
     talon_thread = threading.Thread(target=talon_thread_handler, args=(talon_server_socket, task_queue))
     
     # Start the threads
